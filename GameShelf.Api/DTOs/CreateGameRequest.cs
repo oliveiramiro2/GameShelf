18bef010ce991eae2af2d5788a75876a5a2736dc
Ramiro <ramiromares95@gmail.com>
2026-09-22 19:40:08 -0300

    Add data annotations for validation in CreateGameRequest

--- GameShelf.Api/DTOs/CreateGameRequest.cs
+++ GameShelf.Api/DTOs/CreateGameRequest.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameShelf.Api.DTOs;
 
 public class CreateGameRequest
 {
+  [Required]
   public string Title { get; set; } = string.Empty;
+
+  [Required]
   public string Genre { get; set; } = string.Empty;
+
+  [Range(1950, 2026)]
   public int ReleaseYear { get; set; }
 }
